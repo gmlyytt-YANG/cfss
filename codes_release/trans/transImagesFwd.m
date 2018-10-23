85e6ff57ec213b15993254dec58d5e73166f3bf1
gmlyytt-YANG <gmlyytt@outlook.com>
2018-10-23 15:41:41 +0800

    add code

---
+++ codes_release/trans/transImagesFwd.m
@@ -0,0 +1,28 @@
+% Codes for CVPR-15 work `Face Alignment by Coarse-to-Fine Shape Searching'
+% Any question please contact Shizhan Zhu: zhshzhutah2@gmail.com
+% Released on July 25, 2015
+
+function newImages = transImagesFwd(oldImages,T,rows,cols)
+%newImages = transImagesFwd(oldImages)
+%   images are all cell array.
+
+m = length(oldImages);
+if m~=length(T),error('size(oldImages,1) must equal to length(T)!');end;
+
+% if (m<100) || (parpool('size')==0)
+%     newImages = arrayfun(@(i)imtransform(oldImages{i},T(i),'XData',...
+%         [1 cols],'YData',[1 rows]),1:m,'UniformOutput',false);
+%     if (m>=100)
+%         warning('Please launch parpool to speed up your program!');
+%     end
+% else
+newImages = cell(m,1);
+parfor i = 1:m
+    newImages{i} = imtransform(oldImages{i},T(i),'XData',...
+        [1 cols],'YData',[1 rows]);
+end
+% end
+for i = 1:m, if size(newImages{i},1)~=rows || size(newImages{i},2)~=cols, newImages{i} = imresize(newImages{i},[rows,cols]);end;end;
+
+end
+
