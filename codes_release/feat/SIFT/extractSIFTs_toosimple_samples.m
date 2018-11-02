20a1c57fcbd4297856e938c256f466f0e63b8b0a
gmlyytt-YANG <gmlyytt@outlook.com>
2018-11-02 20:03:56 +0800

    add extractSIFTS_toosimple_samples.m

--- codes_release/feat/SIFT/extractSIFTs_toosimple_samples.m
+++ codes_release/feat/SIFT/extractSIFTs_toosimple_samples.m
@@ -2,7 +2,7 @@
 % Any question please contact Shizhan Zhu: zhshzhutah2@gmail.com
 % Released on July 25, 2015
 
-function featOri_batch = extractSIFTs_toosimple_samples(images,currentPose_samples,iter,featInfo, level)
+function featOri_batch = extractSIFTs_toosimple_samples(images,currentPose_samples,iter,featInfo, level, mode)
 
 m = length(images);
 assert(size(currentPose_samples,1) == m);
@@ -11,7 +11,7 @@ pose = currentPose_samples(:,:,1);
 for i = 2:samplings
     pose = mergePose(pose,currentPose_samples(:,:,i));
 end;
-featOri_total = extractSIFTs_toosimple(images,pose,iter,featInfo, level, 'extractSIFTs_toosimple_samples');
+featOri_total = extractSIFTs_toosimple(images,pose,iter,featInfo, level, 0, 'extractSIFTs_toosimple_samples', mode);
 featOri_batch = reshape(featOri_total,[m,size(featOri_total,2) / samplings,samplings]);
 
 end
